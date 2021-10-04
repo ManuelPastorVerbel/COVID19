0d3d2717c22104b8b1ac3229f347e2c46b34b0fe
ManuelPastorVerbel <manuelverbel@gmail.com>
2021-10-03 20:56:55 -0500

    commit crud directivo

--- COVID.App.FrontEnd/Pages/Estudiantes/EliminarEstudiante.cshtml.cs
+++ COVID.App.FrontEnd/Pages/Estudiantes/EliminarEstudiante.cshtml.cs
@@ -9,8 +9,21 @@ namespace COVID.App.FrontEnd.Pages
 {
     public class EliminarEstudianteModel : PageModel
     {
-        public void OnGet()
+        private static IRepositorioEstudiante _repoEstudiante = new RepositorioEstudiante(new Persistencia.AppContext());
+        
+        [BindProperty]
+        public Estudiante estudiante{get;set;}
+
+        public IActionResult OnGet(int idEstudiante)
         {
+            estudiante = _repoEstudiante.GetEstudiante(idEstudiante);
+            return Page();
         }
+        public IActionResult OnPost()
+        {
+            _repoProfesor.DeleteProfesor(estudiante.id);
+            return RedirectToPage("./ListaEstudiantes");
+        }
+   
     }
 }
